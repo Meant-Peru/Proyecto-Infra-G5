9e7e8d6486deb3a920057fd49cb3aaa3e3bb1dae
Jose_Urda <urdanivia691@gmail.com>
2025-10-05 16:56:48 -0500

    Actualización infraestructura v1

--- aks.tf
+++ aks.tf
@@ -1,3 +1,7 @@
+locals {
+  idapp = "AppG5" # Apellido
+}
+
 resource "azurerm_resource_group" "rg_01" {
   name     = var.resource_group_name
   location = var.location
@@ -18,12 +22,31 @@ resource "azurerm_kubernetes_cluster" "aks_01" {
   identity {
     type = "SystemAssigned"
   }
-
+  
   tags = {
     Environment = var.environment
   }
+
+  role_based_access_control_enabled = true
 }
 
+## ACR
+resource "azurerm_container_registry" "acr" {
+   name = "acr${local.idapp}"
+   location = azurerm_resource_group.rg_01.location  
+   resource_group_name = azurerm_resource_group.rg_01.name 
+   sku = "Basic" 
+   admin_enabled = false 
+   }
+
+## Dar permiso al AKS para extraer imágenes del ACR 
+resource "azurerm_role_assignment" "aks_acr" { 
+  principal_id = azurerm_kubernetes_cluster.aks_01.kubelet_identity[0].object_id 
+  role_definition_name = "AcrPull" 
+  scope = azurerm_container_registry.acr.id 
+  }
+
+## Configuración del proveedor Kubernetes
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.aks_01.kube_config[0].client_certificate
   sensitive = true
